4bb91adb98232399fcafb9de1b960a8091acebc5
Anton Korzunov <thekashey@gmail.com>
2021-07-11 11:51:05 +1000

    update eslint

--- static/.eslintrc.js
+++ static/.eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'prettier', 'import'],
   rules: {
+    '@typescript-eslint/ban-ts-comment': 0,
     '@typescript-eslint/ban-ts-ignore': 0,
     '@typescript-eslint/no-var-requires': 0,
     '@typescript-eslint/camelcase': 0,
@@ -18,6 +19,52 @@ module.exports = {
           order: 'asc',
         },
         groups: ['builtin', 'external', 'internal', ['parent', 'index', 'sibling']],
+      },
+    ],
+    "padding-line-between-statements": [
+      "error",
+      // IMPORT
+      {
+        blankLine: "always",
+        prev: "import",
+        next: "*",
+      },
+      {
+        blankLine: "any",
+        prev: "import",
+        next: "import",
+      },
+      // EXPORT
+      {
+        blankLine: "always",
+        prev: "*",
+        next: "export",
+      },
+      {
+        blankLine: "any",
+        prev: "export",
+        next: "export",
+      },
+      {
+        blankLine: "always",
+        prev: "*",
+        next: ["const", "let"],
+      },
+      {
+        blankLine: "any",
+        prev: ["const", "let"],
+        next: ["const", "let"],
+      },
+      // BLOCKS
+      {
+        blankLine: "always",
+        prev: ["block", "block-like", "class", "function", "multiline-expression"],
+        next: "*",
+      },
+      {
+        blankLine: "always",
+        prev: "*",
+        next: ["block", "block-like", "class", "function", "return", "multiline-expression"],
       },
     ],
   },
